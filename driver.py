40e61a59d1865cc4593fbabd665fa7bb2ad61dc3
Doug Cichon <djcichon@gmail.com>
2017-08-27 22:08:30 -0400

    Change driver back to sigmoid

--- driver.py
+++ driver.py
@@ -6,7 +6,7 @@ training, test = mnist.load()
 
 n = network.Network()
 n.add_layer(28*28)
-n.add_layer(100, ReLU)
+n.add_layer(100, Sigmoid)
 n.add_layer(10, Sigmoid)
 
 n.SGD(training, test, 100, 0.1, 5, 10)
